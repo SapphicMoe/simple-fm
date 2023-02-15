15b57f3bfc48520bca628b58a0cb03c45915e78d
Chloe <solely@riseup.net>
2023-02-15 18:57:37 +0500

    fix: allow for multiple tracks to be searched

--- lib/track.ts
+++ lib/track.ts
@@ -6,30 +6,35 @@ class Track {
   constructor(private readonly token: string) {}
 
   /**
-   * Fetches and returns information on a track.
+   * Search for a track by name.
    * @param trackName - The name of the track.
+   * @param limit - The number of results to fetch per page. Defaults to 30.
+   * @param page - The page number to fetch. Defaults to the first page.
    * */
-  async fetch(trackName: string): Promise<TrackType> {
+  async search(trackName: string, limit = 30, page = 1): Promise<TrackType[]> {
     const {
       results: { trackmatches },
     } = await request<TrackSearchResponse>({
       method: 'track.search',
       track: trackName,
       api_key: this.token,
-      limit: 1,
+      limit,
+      page,
     });
 
-    const [track] = trackmatches.track;
+    const { track } = trackmatches;
 
-    return {
-      name: track.name,
-      artist: track.artist,
-      url: track.url,
-      stats: {
-        listeners: track.listeners,
-      },
-      image: track.image.find((i) => i.size === 'extralarge')?.['#text'],
-    };
+    return track.map((track) => {
+      return {
+        name: track.name,
+        artist: track.artist,
+        url: track.url,
+        stats: {
+          listeners: track.listeners,
+        },
+        image: track.image.find((i) => i.size === 'extralarge')?.['#text'],
+      };
+    });
   }
 }
 
