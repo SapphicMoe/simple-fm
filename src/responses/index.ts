d049feefec87c07f39f34bae8291c7d6269006d3
Chloe <solely@riseup.net>
2023-07-01 21:09:45 +0500

    refactor: add trackResponse interface

--- src/responses/index.ts
+++ src/responses/index.ts
@@ -1,3 +1,12 @@
+import type { Track } from '~/index.js';
+
+export declare interface TrackResponse extends Track {
+  duration: string;
+  '@attr': {
+    rank: number;
+  };
+}
+
 export * from '@responses/album.response.js';
 export * from '@responses/artist.response.js';
 export * from '@responses/chart.response.js';
