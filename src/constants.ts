94fa9dc9c2964f47326c9e0c466142d91c34f2bf
GitHub Action <action@github.com>
2024-03-04 14:32:13 +0000

    chore: update version to 1.7.2

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const PACKAGE_VERSION = '1.7.0';
+const PACKAGE_VERSION = '1.7.2';
 
 export const BASE_URL = 'https://ws.audioscrobbler.com/2.0';
 export const USER_AGENT = `simple-fm (v${PACKAGE_VERSION}) - a simple & lightweight Last.fm library in TypeScript (https://github.com/solelychloe/simple-fm)`;
